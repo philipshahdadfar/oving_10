91d73b13a9181736ce2e59c6f774a22af404a431
philipshahdadfar <93126708+philipshahdadfar@users.noreply.github.com>
2021-11-16 09:39:56 +0100

    Update oving_10.py

--- oving_10.py
+++ oving_10.py
@@ -1,12 +1,23 @@
 
 
+
+class Spiller:
+    def __init__(self, navn, poengsum=0, svar=0):
+        self.navn = navn
+        self.poengsum = poengsum
+        self.svar = svar
+        
+        
+
 class Flervalgssporsmaal:
     
     def __init__(self, sporsmaal, riktig_svar, valg):
         self.__sporsmaal = sporsmaal
         self.__valg = valg
         self.__riktig_svar = riktig_svar
-    
+        
+        
+        
     @property
     def sporsmaal(self):
         return self.__sporsmaal
@@ -37,65 +48,92 @@ class Flervalgssporsmaal:
         tekst = f"Korrekt svar: {self.valg[self.__riktig_svar]}"
         
         return tekst
-
-#############################################################
     
+#############################################################
+
 def lag_sporsmaal():
     sporsmaalene = []
-    
+        
     file = open("sporsmaalsfil.txt", "r", encoding="UTF8")
-
+    
     for line in file:
-    
+        
         stripped_line = line.strip()
-    
+        
         stripped_line = stripped_line.replace("[","")
         stripped_line = stripped_line.replace("]","")
-            
-        split_line = stripped_line.split(":")
-            
-        #print(split_line)
                 
-        
+        split_line = stripped_line.split(":")
+                
+        #print(split_line)
+                    
+            
         question = split_line[0]
-        
+            
         answer = int(split_line[1])
-        
+            
         options = split_line[2]
-    
+        
         opt1 = options.split(",")
-    
-    
+        
+        
         sporsmaalene.append(Flervalgssporsmaal(question, answer, opt1))
-
+    
     return sporsmaalene
 
-############################################################
-if __name__ == "__main__":
-    sporsmaalene = lag_sporsmaal()
-    riktig_svar1 = 0
-    riktig_svar2 = 0
 
+def lag_spillerne():
+        spillerne = []
+        antall_spillere = int(input("Antall spillere: "))
+        for i in range(antall_spillere):
+            navn = input(f"Navnet til spiller {i}: ")
+            spilleren = Spiller(navn)
+            spillerne.append(spilleren)
+        return spillerne
+
+############################################################
+
+if __name__ == "__main__":
+    
+    sporsmaalene = lag_sporsmaal()
+    spillerne = lag_spillerne()
+    
+    mest_poeng = None
+    
+    
     for sporsmaal in sporsmaalene:
         print(sporsmaal)
-        spiller1 = input("Spiller 1: ")
-        spiller2 = input("Spiller 2: ")
-        spiller1 = int(spiller1)
-        spiller2 = int(spiller2)
-        
-        if sporsmaal.sjekk_svar(spiller1):
-            print("Spiller 1: Riktig!\n")
-            riktig_svar1 += 1
-        else:
-            print("Spiller 1: Feil!\n")
+        for spiller in spillerne:
+            spiller.svar = int(input(f"{spiller.navn}: "))
             
-        if sporsmaal.sjekk_svar(spiller2):
-            print("Spiller 2: Riktig!\n")
-            riktig_svar2 += 1
-            print(sporsmaal.korrekt_svar_tekst() + "\n")
-        else:
-            print("Spiller 2: Feil!\n")
-            print(sporsmaal.korrekt_svar_tekst() + "\n")
-    print(f"Spiller 1 har {riktig_svar1} riktige av {len(sporsmaalene)} mulige.")
-    print(f"Spiller 2 har {riktig_svar2} riktige av {len(sporsmaalene)} mulige.")
-    ################################################################
+        for spiller in spillerne:    
+            if sporsmaal.sjekk_svar(spiller.svar):
+                print(f"{spiller.navn}: Riktig!\n")
+                spiller.poengsum +=1
+            else:
+                print(f"{spiller.navn}: Feil!\n")
+                
+    for spiller in spillerne:
+        if (mest_poeng is None or spiller.poengsum > mest_poeng):
+            mest_poeng = spiller.poengsum
+                
+            print(f"Vinner: {spiller.navn}")
+            
+        
+            
+            
+            
+           
+    
+            
+            
+            
+            
+        
+  
+    
+    
+    
+    
+    
+    
